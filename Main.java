7949dfa065d8ac9b524fc3f75130b427fb386758
Sebastian Rataj <100698720+SRataj1005@users.noreply.github.com>
2022-03-26 13:54:47 +0000

    brak potegi i dziala legancko

--- Main.java
+++ Main.java
@@ -1,20 +1,18 @@
-import java.lang.Math;
+import static java.lang.Math.*;
 
 class Main {
   public static void main(String[] args) 
   {
     int[] tablica = new int[10];
-    int podstawa = 2;
     for(int i = 0; i < 10; i++)
       {
-        tablica[i] = Math.pow(podstawa, i);
+        tablica[i] = 2 * i;
+        System.out.println(tablica[i]);
       }
-        for(int j = 10; 0 < j; j--)
+    for(int j = 9; 0 < j; j--)
       {
         System.out.println(tablica[j]);
       }
-
-    
-    
-  }
+    }
 }
+  
